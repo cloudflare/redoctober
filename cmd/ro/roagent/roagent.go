22cc69fbe03d8604fa4f7d4a3c93153fb2ceb500
Mahrud Sayrafi <mahrud@berkeley.edu>
2018-02-05 18:40:13 -0800

    New functions (AddROSigner, etc.) and data structures for ROAgent.
    Almost compatible with ssh-add now.

--- cmd/ro/roagent/roagent.go
+++ cmd/ro/roagent/roagent.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"errors"
+	"log"
 	"io"
 
 	"github.com/cloudflare/redoctober/client"
@@ -15,12 +16,18 @@ import (
 	"golang.org/x/crypto/ssh/agent"
 )
 
-type ROSigner struct {
+type ROAgent struct {
 	server       *client.RemoteServer
-	pub          ssh.PublicKey
-	encryptedKey []byte
 	user         string
 	pswd         string
+	keyring      []*ROSigner
+}
+
+type ROSigner struct {
+	agent        *ROAgent
+	pub          ssh.PublicKey
+	rawKey       []byte
+	encryptedKey []byte
 }
 
 func (signer ROSigner) PublicKey() ssh.PublicKey {
@@ -28,14 +35,15 @@ func (signer ROSigner) PublicKey() ssh.PublicKey {
 }
 
 func (signer ROSigner) Sign(rand io.Reader, msg []byte) (signature *ssh.Signature, err error) {
+	// TODO encryptedKey vs rawKey
 	req := core.SSHSignWithRequest{
-		Name:     signer.user,
-		Password: signer.pswd,
+		Name:     signer.agent.user,
+		Password: signer.agent.pswd,
 		Data:     signer.encryptedKey,
 		TBSData:  msg,
 	}
 
-	resp, err := signer.server.SSHSignWith(req)
+	resp, err := signer.agent.server.SSHSignWith(req)
 	if err != nil {
 		return nil, err
 	}
@@ -55,70 +63,116 @@ func (signer ROSigner) Sign(rand io.Reader, msg []byte) (signature *ssh.Signatur
 	return &sshSignature, nil
 }
 
-type ROAgent struct {
-	signer ROSigner
-}
-
 // NewROAgent creates a new SSH agent which forwards signature requests to the
 // provided remote server
-func NewROAgent(server *client.RemoteServer, pubKey ssh.PublicKey, encryptedPrivKey []byte, user, pswd string) agent.Agent {
-	return &ROAgent{
-		ROSigner{
-			server,
-			pubKey,
-			encryptedPrivKey,
-			user,
-			pswd,
-		},
+func NewROAgent(server *client.RemoteServer, pubKey ssh.PublicKey, encryptedPrivKey []byte, user, pswd string) (agent.Agent, error) {
+	// FIXME these arguments are extra
+	roagent := &ROAgent{
+		server,
+		user,
+		pswd,
+		[]*ROSigner{},
 	}
+
+	err := roagent.AddROSigner(pubKey, encryptedPrivKey)
+	if err != nil {
+		return nil, errors.New("failed to add new signer to the ROAgent")
+	}
+
+	return roagent, nil
 }
 
-// RemoveAll has no effect for the ROAgent
+// NewROSigner adds a new SSH identity to the ROAgent
+func (r *ROAgent) AddROSigner(pubKey ssh.PublicKey, encryptedPrivKey []byte) error {
+	rosigner := &ROSigner{
+		agent:        r,
+		pub:          pubKey,
+		encryptedKey: encryptedPrivKey,
+	}
+	r.keyring = append(r.keyring, rosigner)
+	return nil
+}
+
+// RemoveAll empties ROAgent's keyring
 func (r *ROAgent) RemoveAll() error {
+	r.keyring = []*ROSigner{}
 	return nil
 }
 
-// Remove has no effect for the ROAgent
+// Removes the first matching key from ROAgent's keyring
 func (r *ROAgent) Remove(key ssh.PublicKey) error {
-	return nil
+	wanted := key.Marshal()
+	for i, signer := range r.keyring {
+		if bytes.Equal(signer.PublicKey().Marshal(), wanted) {
+			// Order is not preserved
+			r.keyring[i] = r.keyring[0] 
+			r.keyring = r.keyring[1:]
+			log.Println("signer was removed")
+			return nil
+		}
+	}
+	return errors.New("could not remove signer")
 }
 
-// Lock has no effect for the ROAgent
+// Locks the ROAgent by removing the password
+// TODO should this encrypt the password instead?
 func (r *ROAgent) Lock(passphrase []byte) error {
-	return nil
+	if bytes.Equal(passphrase, []byte(r.pswd)) {
+		r.pswd = ""
+		return nil
+	}
+	return errors.New("could not lock the agent")
 }
 
-// Unlock has no effect for the ROAgent
+// Unlocks the ROAgent by adding the password
+// FIXME ask papa RO if the password is correct
 func (r *ROAgent) Unlock(passphrase []byte) error {
+	r.pswd = string(passphrase)
 	return nil
 }
 
 // List returns the identities known to the agent.
 func (r *ROAgent) List() ([]*agent.Key, error) {
-	return []*agent.Key{
-		{
-			Format:  r.signer.PublicKey().Type(),
-			Blob:    r.signer.PublicKey().Marshal(),
-			Comment: "Red October encrypted SSH key",
-		},
-	}, nil
+	list := make([]*agent.Key, len(r.keyring))
+	for i, signer := range r.keyring {
+		list[i] = &agent.Key{
+			Format:  signer.PublicKey().Type(),
+			Blob:    signer.PublicKey().Marshal(),
+			Comment: r.user,
+		}
+	}
+	return list, nil
 }
 
 // Add has no effect for the ROAgent
+// FIXME
 func (r *ROAgent) Add(key agent.AddedKey) error {
+	signer, _ := ssh.NewSignerFromKey(key.PrivateKey)
+	rosigner := &ROSigner{
+		pub:          signer.PublicKey(),
+		encryptedKey: nil, //[]byte
+	}
+	r.keyring = append(r.keyring, rosigner)
+	log.Println("new signer was added")
 	return nil
 }
 
 // Sign returns a signature for the data.
 func (r *ROAgent) Sign(key ssh.PublicKey, data []byte) (*ssh.Signature, error) {
 	wanted := key.Marshal()
-	if bytes.Equal(r.signer.PublicKey().Marshal(), wanted) {
-		return r.signer.Sign(rand.Reader, data)
+	for _, signer := range r.keyring {
+		if bytes.Equal(signer.PublicKey().Marshal(), wanted) {
+			return signer.Sign(rand.Reader, data)
+		}
 	}
-	return nil, errors.New("wrong key requested")
+	return nil, errors.New("requested key was not found on keyring")
 }
 
 // Signers returns signers for all the known keys.
 func (r *ROAgent) Signers() ([]ssh.Signer, error) {
-	return []ssh.Signer{r.signer}, nil
+	list := make([]ssh.Signer, len(r.keyring))
+	for i, signer := range r.keyring {
+		list[i] = ssh.Signer(signer)
+	}
+	return list, nil
 }
